f21541ddec42d7f2525d7d16588b71a149715534
915-Petruta-Razvan <razvan.petruta@stud.ubbcluj.ro>
2024-04-02 21:18:28 +0300

    update

--- src/test/java/org/example/InClassTestCase.java
+++ src/test/java/org/example/InClassTestCase.java
@@ -1,18 +1,55 @@
 package org.example;
 
+import domain.Student;
 import org.junit.Test;
-import repository.StudentFileRepository;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.ValidationException;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class InClassTestCase {
-    StudentFileRepository repo = new StudentFileRepository("fisiere/Studenti.txt");
+    StudentValidator studentValidator = new StudentValidator();
+    TemaValidator temaValidator = new TemaValidator();
+    String filenameStudent = "fisiere/Studenti.xml";
+    String filenameTema = "fisiere/Teme.xml";
+    String filenameNota = "fisiere/Note.xml";
+    StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+    TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+    NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+    NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+    Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
 
     @Test
     public void addStudentWithValidGrupa() {
-        assert true;
+        Student testStudent = new Student("7890", "Test Student", 935, "teststudent@gmail.com");
+
+        try {
+            service.addStudent(testStudent);
+        } catch (ValidationException ve) {
+            System.out.println(ve.getMessage());
+            fail();
+        }
+
+        assert(service.findStudent("7890") != null);
     }
 
     @Test
     public void addStudentWithInvalidGrupa() {
-        assert true;
+        Student testStudent = new Student("9876", "Test Student", -10, "teststudent@gmail.com");
+
+        try {
+            service.addStudent(testStudent);
+            fail();
+        } catch (ValidationException ve) {
+            System.out.println(ve.getMessage());
+            assertTrue(true);
+        }
     }
 }
